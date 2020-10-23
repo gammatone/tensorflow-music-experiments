416cf2a826960215f0e8ea9ce4d2e8ffb12b76f0
gammatone <58189042+gammatone@users.noreply.github.com>
2020-10-24 01:22:52 +0200

    Add saving step in filter8_RNN.py

--- python/scripts/emulations/filter8_RNN.py
+++ python/scripts/emulations/filter8_RNN.py
@@ -23,8 +23,10 @@ The use of RNN type is motivated by its temporal behaviour which enables to make
 import os, sys
 
 import numpy as np
-import tensorflow as tf
+import pickle
 
+import tensorflow as tf
+from tensorflow.keras.models import load_model
 
 file_dir = os.path.dirname(os.path.realpath(__file__))
 root_dir = os.path.abspath(os.path.join(file_dir, os.pardir))
@@ -56,15 +58,189 @@ def get_RNN_model(in_length, out_length, n_features, stateful=False, batch_size=
                             )
     return model
 
+def training_routine(pkl_dir, input_signal_keys, output_signal_keys,
+                    optimizer=optimizer_call(lr=1e-3), loss_metric="mse",
+                    audio_out_dir=None, model_root_save_dir=None,
+                    pkl_filenames=("train.pkl", "validation.pkl", "evaluation.pkl")
+                    ):
+    """
+    Training function:
+        - Load train & validation dataset
+        - Create many-to-many RNN (e.g. IO_size=2048; hop_size=512)
+        - Train RNN in stateless mode (i.e. truncated backprop for each chunks of length IO_size)
+        - Evaluate trained model
+        - Create audio file corresponding to the infered data from evaluation dataset
+        - Save model and results (in .h5 and .pkl files)
+    """
+    # Define model handler for training
+    my_model_trainer = KerasBufferizedNNHandler(pkl_dir, input_signal_keys, output_signal_keys,
+                                                optimizer, loss_metric,
+                                                IO_SEQ_LENGTH, HOP_SIZE, IO_SEQ_LENGTH,
+                                                need_training=True,
+                                                train_filename=pkl_filenames[0],
+                                                val_filename=pkl_filenames[1],
+                                                eval_filename=pkl_filenames[2],
+                                                )
+
+    if NEED_PLOT:
+        plot_by_key(my_model_trainer.train_dict, my_model_trainer.train_dict.keys(), title="Extract from train dataset",
+                    start_idx=int(5e4), end_idx=int(5.1e4))
+        plot_by_key(my_model_trainer.val_dict, my_model_trainer.val_dict.keys(), title="Extract from validation dataset",
+                    start_idx=int(2e4), end_idx=int(2.1e4))
+    my_model_trainer.prepare_datasets()
+    # Define STATELESS RNN model
+    model = get_RNN_model(in_length=IO_SEQ_LENGTH, out_length=IO_SEQ_LENGTH, n_features=N_FEATURES,
+                            stateful=False,
+                            )
+    model.summary()
+
+    ## MODEL TRAINING ##
+    training_state = my_model_trainer.train_model(model, epochs=EPOCHS, batch_size=BATCH_SIZE,
+                                                    val_freq=VALIDATION_FREQ)
+    # Define a dict for training losses
+    training_losses = {}
+    training_losses["loss"] = np.array(training_state.history["loss"])
+    training_losses["val_loss"] = np.repeat(np.array(training_state.history["val_loss"]), VALIDATION_FREQ)
+    plot_by_key(training_losses, 
+            ["loss", "val_loss",],
+            title="Losses value during training",
+            )
+
+    ## MODEL EVALUATION ##
+    # Evaluate trained model (i.e. many-to-many samples model)  
+    print("Evaluating trained model")
+    eval_score = my_model_trainer.evaluate_model(model, need_plot=NEED_PLOT)
+    print("Evaluation score = {}".format(eval_score))
+
+    print("Inference on trained model")
+    # Predict output of evaluation dataset inputs from trained model
+    eval_predictions = my_model_trainer.predict(model, my_model_trainer.x_eval)
+    print("Saving inference output to audio file")
+
+    ## AUDIO FILE WRITING ##
+    if audio_out_dir is not None:
+        # Save to audio file
+        if not os.path.exists(audio_out_dir):
+            os.makedirs(audio_out_dir)
+        savefilepath = os.path.join(audio_out_dir, "infered_data_trained_model.wav")
+        # Use numpy asfortranarray() function to ensure Fortran contiguity on the array
+        librosa_write_wav(np.asfortranarray(eval_predictions), savefilepath, sr=SAMPLE_RATE)
+
+    ## MODEL SAVING ##
+    model_save_dir = None
+    if model_root_save_dir is not None:
+        # Save model and results into a sub directory specified by current data/time
+        import datetime
+        model_save_dir = os.path.join(model_root_save_dir, datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S'))
+        os.makedirs(model_save_dir)
+        # Save useful constants from training as pickle
+        result_dict = {}
+        result_dict["sample_rate"] = SAMPLE_RATE
+        result_dict["epochs"] = EPOCHS
+        result_dict["batch_size"] = BATCH_SIZE
+        result_dict["hop_size"] = HOP_SIZE
+        result_dict["loss_metric"] = loss_metric
+        result_dict["optimizer"] = optimizer
+        result_dict["val_freq"] = VALIDATION_FREQ
+        result_dict["training_losses"] = training_losses
+        with open(os.path.join(model_save_dir,"training_results.pkl"), "wb") as f:
+            pickle.dump(result_dict, f)
+        # Save model in the sub directory
+        model.save(os.path.join(model_save_dir,'trained_model.h5'),)
+
+    return model_save_dir
+
+def testing_routine(savedmodel_dir, pkl_dir, input_signal_keys, output_signal_keys,
+                    audio_out_dir=None, test_pkl_filename="evaluation.pkl",
+                    ):
+    """
+    Testing function:
+        - Load a pretrained many-to-many RNN Keras model
+        - Create one-to-one stateful RNN (i.e. the real-time implementation model)
+        with same graph than pretrained model
+        - Copy weight from the pretrained model
+        - Train RNN in stateless mode (i.e. truncated backprop for each chunks of length IO_size)
+        - Evaluate stateful model
+        - Create audio file corresponding to the infered data from a test dataset
+    """
+    ## PRETRAINED MODEL LOADING ##
+    # Load pretrained stateless model
+    for file in os.listdir(savedmodel_dir):
+        if file == "trained_model.h5":
+            pretrained_model = load_model(os.path.join(savedmodel_dir, file),)
+            break
+    # Load training infos from saved dictionary
+    with open(os.path.join(savedmodel_dir, "training_results.pkl"), "rb") as f:
+        training_results_dict = pickle.load(f)
+    # Deduce optimizer and loss metric to be able to compile real-time model later
+    optimizer = training_results_dict["optimizer"]
+    loss_metric = training_results_dict["loss_metric"]
+
+    ## REAL-TIME MODEL CREATION ##
+    # Define model handler for testing one-to-one stateful model:
+    # means only evaluation dataset (no train and validation datasets)
+    # means io_seq_length=1; hop_size=1; 
+    my_rt_model_tester = KerasBufferizedNNHandler(pkl_dir, input_signal_keys, output_signal_keys,
+                                                optimizer, loss_metric,
+                                                1, 1, 1,
+                                                need_training=False,
+                                                eval_filename=test_pkl_filename,
+                                                )
+    my_rt_model_tester.prepare_datasets()
+
+    print("\nOne-to-one RNN model (i.e. the one used in real-time) creation according to trained model config")
+    rt_model = get_RNN_model(in_length=1, out_length=1, n_features=N_FEATURES,
+                            stateful=True,
+                            batch_size=1,
+                            )
+    rt_model.summary()
+    # Copy weights from pretrained model
+    training_weights = pretrained_model.get_weights()
+    rt_model.set_weights(training_weights)
+
+    ## REAL-TIME MODEL EVALUATION ##
+    print("Evaluating real-time model")
+    eval_duration_s = 5.
+    eval_score = my_rt_model_tester.evaluate_model(rt_model, batch_size=1,
+                                                    start_end_idxs=(0, int(eval_duration_s * SAMPLE_RATE)),
+                                                    need_plot=NEED_PLOT)
+    print("Evaluation score = {}".format(eval_score))
+    print("Inference on real-time model")
+    # Predict output of evaluation dataset inputs from stateful model
+    eval_predictions = my_rt_model_tester.predict(rt_model, my_rt_model_tester.x_eval, batch_size=1)
+    print("Saving inference output to audio file")
+
+    ## AUDIO FILE WRITING ##
+    # Save to audio file
+    if not os.path.exists(audio_out_dir):
+        os.makedirs(audio_out_dir)
+    savefilepath = os.path.join(audio_out_dir, "infered_data_realtime_model.wav")
+    # Use numpy asfortranarray() function to ensure Fortran contiguity on the array
+    librosa_write_wav(np.asfortranarray(eval_predictions), savefilepath, sr=SAMPLE_RATE)
+
+    return
+
 def main():
     """
     Main routine
     """
+    global NEED_PLOT
+    global SAMPLE_RATE
+    global IO_SEQ_LENGTH, HOP_SIZE, N_FEATURES
+    global EPOCHS, BATCH_SIZE, VALIDATION_FREQ
     # define plot flags
-    need_plot = False
+    NEED_PLOT = False
 
     # define audio parameters
-    sr = 44100
+    SAMPLE_RATE = 44100
+
+    # Training hyper parameters
+    IO_SEQ_LENGTH = 4096
+    HOP_SIZE = 1024
+    N_FEATURES = 3
+    BATCH_SIZE = 32
+    EPOCHS = 500
+    VALIDATION_FREQ = 3
 
     # Define signal keys
     input_signal_keys = ["signal_in", "cutoff", "resonance"]
@@ -85,100 +261,28 @@ def main():
     # Define an audio output directory
     audio_out_dir = os.path.join(audio_dir, "inference")
     my_audio_dirs = (audio_train_dir, audio_val_dir, audio_eval_dir)
+    # Define where to save trained model
+    model_root_save_dir = os.path.join(root_dir, "data/keras_models/RNN")
 
     # Save to pickle if dataset does not exist yet
     # First make sure pickle directories exist
     for i in range(len(my_pkl_filenames)):
         if not os.path.exists(my_pkl_filenames[i]):
             # Create pickle dictionary
-            create_pkl_audio_dataset(my_audio_dirs[i], pkl_dir, my_pkl_filenames[i], keynames=signal_keys, sr=sr)
+            create_pkl_audio_dataset(my_audio_dirs[i], pkl_dir, my_pkl_filenames[i], keynames=signal_keys, sr=SAMPLE_RATE)
 
-    ## DATA FORMATING FOR RNN ##
-    # Training hyper parameters
-    IN_SEQ_LENGTH = 4096
-    HOP_SIZE = 4096
-    N_FEATURES = 3
-    # (Careful: for evaluation, out length should be higher than hop size to recover all the data)
-    OUT_SEQ_LENGTH = 4096
-    BATCH_SIZE = 32
-    EPOCHS = 10
-
-    # Define model trainer
-    my_model_trainer = KerasBufferizedNNHandler(pkl_dir, input_signal_keys, output_signal_keys,
-                                                optimizer_call(lr=1e-3), "mse",
-                                                IN_SEQ_LENGTH, HOP_SIZE, OUT_SEQ_LENGTH,
-                                                epochs=EPOCHS, batch_size=BATCH_SIZE)
-
-    if need_plot:
-        plot_by_key(my_model_trainer.train_dict, my_model_trainer.train_dict.keys(), title="Extract from train dataset",
-                    start_idx=int(5e4), end_idx=int(5.1e4))
-        plot_by_key(my_model_trainer.val_dict, my_model_trainer.val_dict.keys(), title="Extract from validation dataset",
-                    start_idx=int(2e4), end_idx=int(2.1e4))
-    my_model_trainer.prepare_datasets()
-    # Define STATELESS RNN model
-    model = get_RNN_model(in_length=IN_SEQ_LENGTH, out_length=OUT_SEQ_LENGTH, n_features=N_FEATURES,
-                            stateful=False,
-                            )
-    model.summary()
-    # Start model training
-    my_model_trainer.train_model(model)
-
-    ## MODEL TESTING ##
-    # Evaluate trained model (i.e. many-to-many samples model)  
-    print("Evaluating trained model")
-    eval_score = my_model_trainer.evaluate_model(model, need_plot=False)
-    print("Evaluation score = {}".format(eval_score))
-
-    print("Inference on trained model")
-    # Predict output of evaluation dataset inputs from trained model
-    eval_predictions = my_model_trainer.predict(model, my_model_trainer.x_eval)
-    print("Saving inference output to audio file")
-    # Save to audio file
-    if not os.path.exists(audio_out_dir):
-        os.makedirs(audio_out_dir)
-    savefilepath = os.path.join(audio_out_dir, "infered_data_trained_model.wav")
-    # Use numpy asfortranarray() function to ensure Fortran contiguity on the array
-    librosa_write_wav(np.asfortranarray(eval_predictions), savefilepath, sr=sr)
-
-    # Redefine a STATEFUL new model to get a one-to-one sample RNN model
-    print("\nOne-to-one sample model creation from trained model (i.e. the one used in real-time)")
-
-    # Define model trainer
-    my_stateful_model_handler = KerasBufferizedNNHandler(pkl_dir, input_signal_keys, output_signal_keys,
-                                                optimizer_call(lr=1e-3), "mse",
-                                                1, 1, 1,
-                                                )
-    my_stateful_model_handler.prepare_datasets()
-    stateful_model = get_RNN_model(in_length=1, out_length=1, n_features=N_FEATURES,
-                            stateful=False,
-                            batch_size=1,
-                            )
-    stateful_model.summary()
-    # Copy weights from trained model
-    training_weights = model.get_weights()
-    stateful_model.set_weights(training_weights)
-    # Do evaluation
-    stateful_model.compile(optimizer_call(lr=1e-3), "mse",)
-    print("Evaluating real-time model")
-    eval_duration_s = 0.1
-    eval_score = my_stateful_model_handler.evaluate_model(stateful_model, batch_size=1,
-                                                    start_end_idxs=(0, int(eval_duration_s * sr)),
-                                                    need_plot=need_plot)
-    print("Evaluation score = {}".format(eval_score))
+    # Train, evaluate and save RNN stateless model
+    savedmodel_dir = training_routine(pkl_dir, input_signal_keys, output_signal_keys,
+                                    optimizer=optimizer_call(lr=1e-3), loss_metric="mse",
+                                    audio_out_dir=audio_out_dir, model_root_save_dir=model_root_save_dir,
+                                    )
 
 
-    print("Inference on real-time model")
-    # Predict output of evaluation dataset inputs from stateful model
-    eval_predictions = my_stateful_model_handler.predict(stateful_model, my_stateful_model_handler.x_eval, batch_size=1)
-    print("Saving inference output to audio file")
-    # Save to audio file
-    if not os.path.exists(audio_out_dir):
-        os.makedirs(audio_out_dir)
-    savefilepath = os.path.join(audio_out_dir, "infered_data_realtime_model.wav")
-    # Use numpy asfortranarray() function to ensure Fortran contiguity on the array
-    librosa_write_wav(np.asfortranarray(eval_predictions), savefilepath, sr=sr)
 
-    return
+    testing_routine(savedmodel_dir, pkl_dir,
+                    input_signal_keys, output_signal_keys,
+                    audio_out_dir=audio_out_dir, test_pkl_filename="evaluation.pkl",
+                    )
 
 
 if __name__ == "__main__":
